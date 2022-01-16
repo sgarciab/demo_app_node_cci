d5a1f48a9d72ed36207083116dd9cf8f396ab7c1
Xtreme LINUX <santiagogarciabautista@gmail.com>
2022-01-15 20:27:40 -0500

    second commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,9 @@
 sudo apt update -y
 sudo apt install git -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install v16.13.2
 sudo apt install unzip -y
 npm install pm2 -g 
