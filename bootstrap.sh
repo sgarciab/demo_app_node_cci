8a4ef736d1149cf2765800fbe4788705347bf27f
Xtreme LINUX <santiagogarciabautista@gmail.com>
2022-01-15 19:55:34 -0500

    second commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@ sudo apt install git -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install v16.13.2
+sudo apt install unzip -y
 npm install pm2 -g 
